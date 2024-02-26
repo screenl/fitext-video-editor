4d386c0e6f68f18b6bdd5f32f323ee5c0db516aa
Ken <kg3209@nyu.edu>
2024-02-26 16:48:55 +0800

    final fixup

--- src/Components/TimeLine.tsx
+++ src/Components/TimeLine.tsx
@@ -3,9 +3,9 @@ import { Rows } from "./TimeLineGadgets/rows";
 
 export default function TimeLine() {
   return (
-    <div className="">
-      <Boxes></Boxes>
+    <div className="flex flex-row justify-center ">
       <Rows></Rows>
+      <Boxes></Boxes>
     </div>
   );
 }
