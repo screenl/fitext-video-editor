ab72ca9a28de14e4508e131675614754809625ce
Ken <kg3209@nyu.edu>
2024-02-26 14:10:02 +0800

    Better UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import TimeLine from "~/Components/TimeLine";
 
 export default function HomePage() {
   return (
-    <div className="m-20 flex justify-center">
+    <div className=" flex justify-center">
       <TimeLine></TimeLine>
     </div>
   );
