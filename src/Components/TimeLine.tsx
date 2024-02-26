f8f72f6f3f998e8d9f5df014daa5fc2df0e415cd
Jerry-812 <jx2498@nyu.edu>
2024-02-26 16:09:09 +0800

    rough rows

--- src/Components/TimeLine.tsx
+++ src/Components/TimeLine.tsx
@@ -1,9 +1,11 @@
 import { Boxes } from "./TimeLineGadgets/ResizableBoxes";
+import { Rows } from "./TimeLineGadgets/rows";
 
 export default function TimeLine() {
   return (
     <div className="">
       <Boxes></Boxes>
+      <Rows></Rows>
     </div>
   );
 }
