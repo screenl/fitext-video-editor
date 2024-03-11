8bd0f862bb1bd32e3ecc740f0b8497b25a9f37ca
Ken <kg3209@nyu.edu>
2024-03-11 14:28:52 +0800

    Fill the trail

--- src/Components/TimeLineGadgets/ResizableBoxes.tsx
+++ src/Components/TimeLineGadgets/ResizableBoxes.tsx
@@ -6,51 +6,73 @@ import {
 
 export function Boxes() {
   return (
-    <ResizablePanelGroup
-      direction="horizontal"
-      className="flex w-full flex-col rounded-lg border"
-    >
-      <ResizablePanel
-        defaultSize={30}
-        className="grid grid-rows-4 justify-items-stretch divide-y"
+    <div className="relative w-[5000px]">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex h-full flex-col rounded-lg border"
       >
-        <div className="flex items-center justify-center text-center">
-          <button className="btn btn-circle btn-outline btn-info text-2xl">
-            +
-          </button>
-        </div>
-        <span className="flex items-center justify-center text-center font-semibold">
-          Sets
-        </span>
-        <span className="flex items-center justify-center text-center font-semibold">
-          Reps
-        </span>
-        <span className="flex items-center justify-center text-center font-semibold">
-          Duration
-        </span>
-      </ResizablePanel>
+        <ResizablePanel
+          defaultSize={30}
+          className="grid grid-rows-4 justify-items-stretch divide-y"
+        >
+          <div className="flex items-center justify-center text-center">
+            <button className="btn btn-circle btn-outline btn-info text-2xl">
+              +
+            </button>
+          </div>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Sets
+          </span>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Reps
+          </span>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Duration
+          </span>
+        </ResizablePanel>
 
-      <ResizableHandle withHandle />
+        <ResizableHandle withHandle />
 
-      <ResizablePanel
-        defaultSize={70}
-        className="grid grid-rows-4 justify-items-stretch divide-y"
-      >
-        <div className="flex items-center justify-center text-center">
-          <button className="btn btn-circle btn-outline btn-info text-2xl">
-            +
-          </button>
-        </div>
-        <span className="flex items-center justify-center text-center font-semibold">
-          Sets
-        </span>
-        <span className="flex items-center justify-center text-center font-semibold">
-          Reps
-        </span>
-        <span className="flex items-center justify-center text-center font-semibold">
-          Duration
-        </span>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+        <ResizablePanel
+          defaultSize={30}
+          className="grid grid-rows-4 justify-items-stretch divide-y"
+        >
+          <div className="flex items-center justify-center text-center">
+            <button className="btn btn-circle btn-outline btn-info text-2xl">
+              +
+            </button>
+          </div>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Sets
+          </span>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Reps
+          </span>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Duration
+          </span>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel
+          defaultSize={30}
+          className="grid grid-rows-4 justify-items-stretch divide-y"
+        >
+          <div className="flex items-center justify-center text-center">
+            <button className="btn btn-circle btn-outline btn-info text-2xl">
+              +
+            </button>
+          </div>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Sets
+          </span>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Reps
+          </span>
+          <span className="flex items-center justify-center text-center font-semibold">
+            Duration
+          </span>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
   );
 }
